87e6d2a062abdca8660a78fccc60c6d4c7df8f88
Rickson Henrique (Xucacodev) <rickson.henrique2018@gmail.com>
2022-08-10 11:39:55 -0300

    intial board adjusts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './Main-components/welcome-page/welcome-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule
